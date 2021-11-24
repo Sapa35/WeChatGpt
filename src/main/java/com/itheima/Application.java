da427503f2d8ad711491f28b46c6d6c5078076b1
Dsapa <Dsapa@itcast.cn>
2021-11-24 20:53:01 +0800

    Helloworld

---
+++ src/main/java/com/itheima/Application.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * @author Dsapa
+ * @date 2021/11/24 20:39
+ * @Description
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/itheima/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dsapa
+ * @date 2021/11/24 20:39
+ * @Description
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping
+    public String test(){
+        System.out.println("Helloworld,fuck u");
+        return "Helloworld,fuck u";
+    }
+}
